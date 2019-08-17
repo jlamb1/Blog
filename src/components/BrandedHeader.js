bfb005b90a7dba1f72edd6e57f494e7bea530e4e
langermank <langermank@gmail.com>
2019-08-17 08:44:35 -0400

    Added CSS for social icons

--- src/components/BrandedHeader.js
+++ src/components/BrandedHeader.js
@@ -22,12 +22,12 @@ export default function BrandedHeader() {
           </li>
           <li className={styles.socialIconSpacing}>
             <a aria-label="github" href="https://github.com/cargurus/Blog">
-              <FontAwesomeIcon role="button" icon={faTwitter} size="lg" />
+              <FontAwesomeIcon role="button" icon={faTwitter} />
             </a>
           </li>
           <li>
             <a aria-label="twitter" href="https://twitter.com/cargurus/">
-              <FontAwesomeIcon role="button" icon={faGithub} size="lg" />
+              <FontAwesomeIcon role="button" icon={faGithub} />
             </a>
           </li>
         </ul>
